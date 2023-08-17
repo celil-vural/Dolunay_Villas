f829c3d758095eef326ae57f7bc6d12002c9f3f5
celil-vural <celilvural.work@gmail.com>
2023-08-17 18:01:25 +0300

    Add Region Validator

--- Service/Concrete/RealEstateManagement/RealEstateRegionService.cs
+++ Service/Concrete/RealEstateManagement/RealEstateRegionService.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
+using Dolunay_Villas.Core.CrossCuttingConcerns.Aspects.Postsharp.ValidationAspect;
 using Entity.Dtos.RealEstateManagement.Region;
 using Entity.Models.RealEstateManagement;
 using Repository.Contracts.RealEstateManagement;
 using Service.Contract.RealEstateManagement;
+using Service.ValidationRules.FluentValidation.RealEstate.Region;
 
 namespace Service.Concrete.RealEstateManagement
 {
@@ -10,6 +12,17 @@ namespace Service.Concrete.RealEstateManagement
     {
         public RealEstateRegionService(IRealEstateRegionRepository baseRepository, IMapper mapper) : base(baseRepository, mapper)
         {
+        }
+
+        [FluentValidationAspect(typeof(RealEstateRegionValidator))]
+        public override int CreateWithDto<TDtoForInsertion>(TDtoForInsertion dtoForInsertion)
+        {
+            return base.CreateWithDto(dtoForInsertion);
+        }
+        [FluentValidationAspect(typeof(RealEstateRegionValidator))]
+        public override void Update(RegionDto dto)
+        {
+            base.Update(dto);
         }
     }
 }
---
+++ Service/ValidationRules/FluentValidation/RealEstate/Region/RealEstateRegionValidator.cs
@@ -0,0 +1,15 @@
+﻿using Entity.Dtos.RealEstateManagement.Region;
+using FluentValidation;
+
+namespace Service.ValidationRules.FluentValidation.RealEstate.Region
+{
+    public class RealEstateRegionValidator : AbstractValidator<RegionDtoForInsertion>
+    {
+        public RealEstateRegionValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required.");
+            RuleFor(x => x.CreatedByUser).NotEmpty().WithMessage("Create by user must not empty.Please log out of the system and log in again. ");
+            RuleFor(x => x.UpdatedByUser).NotEmpty().WithMessage("Update by user must not empty.Please log out of the system and log in again. ");
+        }
+    }
+}
