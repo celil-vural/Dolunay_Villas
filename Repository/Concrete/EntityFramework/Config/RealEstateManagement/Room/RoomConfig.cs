5a3ac50a668bad5183d2b78cc840c6f8b1520ad6
celil-vural <celilvural.work@gmail.com>
2023-08-22 23:34:22 +0300

    Create Room Config

---
+++ Repository/Concrete/EntityFramework/Config/RealEstateManagement/Room/RoomConfig.cs
@@ -0,0 +1,78 @@
+﻿using Entity.Models.RealEstateManagement.Room;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Repository.Concrete.EntityFramework.Config.RealEstateManagement.Room
+{
+    public class RoomConfig : IEntityTypeConfiguration<Rooms>
+    {
+        public void Configure(EntityTypeBuilder<Rooms> builder)
+        {
+            builder.HasData(
+                new Rooms
+                {
+                    Id = 1,
+                    Title_Tr = "1. Yatak Odası",
+                    Title_En = "1. Bedroom",
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                    CreatedByUser = "Default",
+                },
+                new Rooms
+                {
+                    Id = 2,
+                    Title_Tr = "2. Yatak Odası",
+                    Title_En = "2. Bedroom",
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                    CreatedByUser = "Default",
+                },
+                new Rooms
+                {
+                    Id = 3,
+                    Title_Tr = "3. Yatak Odası",
+                    Title_En = "3. Bedroom",
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                    CreatedByUser = "Default",
+                },
+                new Rooms
+                {
+                    Id = 4,
+                    Title_Tr = "4. Yatak Odası",
+                    Title_En = "4. Bedroom",
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                    CreatedByUser = "Default",
+                },
+                new Rooms
+                {
+                    Id = 5,
+                    Title_Tr = "Açık Mutfak",
+                    Title_En = "Open Kitchen",
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                    CreatedByUser = "Default",
+                },
+                new Rooms
+                {
+                    Id = 6,
+                    Title_Tr = "Mutfak",
+                    Title_En = "Kitchen",
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                    CreatedByUser = "Default",
+                },
+                new Rooms
+                {
+                    Id = 7,
+                    Title_Tr = "Salon",
+                    Title_En = "Saloon",
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                    CreatedByUser = "Default",
+                }
+            );
+        }
+    }
+}
