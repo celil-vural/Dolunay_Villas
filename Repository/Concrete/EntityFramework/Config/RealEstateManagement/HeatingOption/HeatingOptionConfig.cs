52aebeef47dc629f1a18a4d36e7cd7d31ee0842c
celil-vural <celilvural.work@gmail.com>
2023-08-22 23:47:55 +0300

    Create Heating Option Config

---
+++ Repository/Concrete/EntityFramework/Config/RealEstateManagement/HeatingOption/HeatingOptionConfig.cs
@@ -0,0 +1,65 @@
+﻿using Entity.Models.RealEstateManagement.HeatingOption;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Repository.Concrete.EntityFramework.Config.RealEstateManagement.HeatingOption
+{
+    public class HeatingOptionConfig : IEntityTypeConfiguration<HeatingOptions>
+    {
+        public void Configure(EntityTypeBuilder<HeatingOptions> builder)
+        {
+            builder.HasData(
+                new HeatingOptions
+                {
+                    Id = 1,
+                    Title_Tr = "Doğalgaz",
+                    Title_En = "Natural Gas",
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                    CreatedByUser = "Default",
+                    UpdatedByUser = "Default"
+                },
+                new HeatingOptions
+                {
+                    Id = 2,
+                    Title_Tr = "Klima",
+                    Title_En = "Air Conditioner",
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                    CreatedByUser = "Default",
+                    UpdatedByUser = "Default"
+                },
+                new HeatingOptions
+                {
+                    Id = 3,
+                    Title_Tr = "Soba",
+                    Title_En = "Stove",
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                    CreatedByUser = "Default",
+                    UpdatedByUser = "Default"
+                },
+                new HeatingOptions
+                {
+                    Id = 4,
+                    Title_Tr = "Şömine",
+                    Title_En = "Fire Place",
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                    CreatedByUser = "Default",
+                    UpdatedByUser = "Default"
+                },
+                new HeatingOptions
+                {
+                    Id = 5,
+                    Title_Tr = "Yerden Isıtma",
+                    Title_En = "Underfloor Heating",
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                    CreatedByUser = "Default",
+                    UpdatedByUser = "Default"
+                }
+            );
+        }
+    }
+}
